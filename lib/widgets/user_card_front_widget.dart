758bbd21ae2b8497ce1afe5e08c7922c0d94e9f7
seonwookkim <seonwooke@naver.com>
2022-01-05 14:27:21 +0900

    2021/1/5/14:27:SW-dev

--- lib/widgets/user_card_front_widget.dart
+++ lib/widgets/user_card_front_widget.dart
@@ -1,7 +1,5 @@
 import 'package:advanced_flutter/models/user_model.dart';
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget userCardFront(UserModel userModel) {
