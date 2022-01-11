bec89061e06f18e308a896c9a629b0f54ccec788
seonwookkim <seonwooke@naver.com>
2022-01-11 19:19:55 +0900

    2021/1/11/19:19:SW-chips_demo

--- lib/pages/interest_test_screen.dart
+++ lib/pages/interest_test_screen.dart
@@ -1,18 +1,23 @@
 import 'package:advanced_flutter/controllers/user_info_controller.dart';
 import 'package:chip_list/chip_list.dart';
 import 'package:chips_choice_null_safety/chips_choice_null_safety.dart';
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class InterestTestScreen extends StatelessWidget {
   final userInfoController = Get.find<UserInfoController>();
-  List<String> tags = [];
-  List<String> options = [
-    'News', 'Entertainment', 'Politics',
-    'Automotive', 'Sports', 'Education',
-    'Fashion', 'Travel', 'Food', 'Tech',
-    'Science',
+  List<String> countList = [
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
   ];
 
   @override
@@ -35,6 +40,33 @@ class InterestTestScreen extends StatelessWidget {
           SizedBox(height: 30,),
           Text('더 알고싶어요\n이대성님을'),
           SizedBox(height: 30,),
+          Expanded(
+            child: FilterListWidget<String>(
+              listData : countList,
+              hideHeaderText: true,
+              onApplyButtonClick: (list) {
+                if (list != null) {
+                  print("Selected items count : ${list.length}");
+                }
+              },
+              choiceChipLabel: (item) {
+                return item.toString();
+              },
+              validateSelectedItem: (list, val) {
+                return list!.contains(val);
+              },
+              onItemSearch: (list, text) {
+                if (list!.any((element) =>
+                  element.toLowerCase().contains(text.toLowerCase()))) {
+                  return list.where((element) =>
+                    element.toLowerCase().contains(text.toLowerCase())).toList();
+                }
+                else {
+                  return [];
+                }
+              },
+            ),
+          ),
         ],
       ),
     );
